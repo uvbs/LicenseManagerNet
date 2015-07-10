1e8fbc6aca5deb4167f03e3e7365608b26c9f7a7
Torsten Werner <werner.torsten@1und1.de>
2015-07-10 13:41:41 +0200

    ADDED all basic crud tests for manufacturerscontroller

--- LicenseManager.Api.Tests/Controllers/ManufacturersControllerTest.cs
+++ LicenseManager.Api.Tests/Controllers/ManufacturersControllerTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LicenseManager.Api.Controllers;
 using System.Web.Http.Results;
 using LicenseManager.Shared.Models;
+using System.Net;
 
 namespace LicenseManager.Api.Tests.Controllers
 {
@@ -10,7 +11,7 @@ namespace LicenseManager.Api.Tests.Controllers
     public class ManufacturersControllerTest
     {
         [TestMethod]
-        public void GetProducts_ShouldReturnAllProducts()
+        public void GetManufacturers_ShouldReturnAllManufacturers()
         {
             var context = new TestLicenseManagerContext();
             context.Manufacturers.Add(new Manufacturer { Id = 1, Name = "Manufacturer 1" });
@@ -27,7 +28,36 @@ namespace LicenseManager.Api.Tests.Controllers
         [TestMethod]
         public void GetManufacturer_ShouldReturnManufacturerWithSameId()
         {
+            var context = new TestLicenseManagerContext();
+            context.Manufacturers.Add(GetDemoManufacturer());
+            var controller = new ManufacturersController(context);
 
+            var result = controller.GetManufacturer(3) as OkNegotiatedContentResult<Manufacturer>;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Content.Id);
+        }
+
+        [TestMethod]
+        public void PutManufacturer_ShouldReturnStatusCode()
+        {
+            var controller = new ManufacturersController(new TestLicenseManagerContext());
+            var item = GetDemoManufacturer();
+
+            var result = controller.PutManufacturer(item.Id, item) as StatusCodeResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(StatusCodeResult));
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void PutManufacturer_ShouldFail_WhenDifferentId()
+        {
+            var controller = new ManufacturersController(new TestLicenseManagerContext());
+
+            var badresult = controller.PutManufacturer(999, GetDemoManufacturer());
+            Assert.IsInstanceOfType(badresult, typeof(BadRequestResult));
         }
 
         [TestMethod]
@@ -42,6 +72,21 @@ namespace LicenseManager.Api.Tests.Controllers
             Assert.AreEqual(result.RouteName, "DefaultApi");
             Assert.AreEqual(result.RouteValues["id"], result.Content.Id);
             Assert.AreEqual(result.Content.Name, item.Name);
+        }
+
+        [TestMethod]
+        public void DeleteManufacturer_ShouldReturnOk()
+        {
+            var context = new TestLicenseManagerContext();
+            var item = GetDemoManufacturer();
+            context.Manufacturers.Add(item);
+            var controller = new ManufacturersController(context);
+
+            var result = controller.DeleteManufacturer(item.Id) as OkNegotiatedContentResult<Manufacturer>;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(item.Id, result.Content.Id);
+
         }
 
         private Manufacturer GetDemoManufacturer()
