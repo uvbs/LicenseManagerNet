35f0220e1e592fe1dae200fd748e876c65ff2c74
Torsten Werner <werner.torsten@1und1.de>
2015-08-26 16:31:42 +0200

    added ComputersController and tests

---
+++ LicenseManager.Api.Tests/Controllers/ComputersControllerTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LicenseManager.Shared;
+using LicenseManager.Api.Controllers;
+
+namespace LicenseManager.Api.Tests.Controllers
+{
+    [TestClass]
+    public class ComputersControllerTest
+    {
+        [TestMethod]
+        public void GetComputers_ShouldReturnAllComputers()
+        {
+            var context = GetDemoContext();
+            var controller = new ComputersController(context);
+
+            var result = controller.GetComputers() as TestComputerDbSet;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Computers.Local.Count, result.Local.Count);
+        }
+
+        [TestMethod]
+        public void GetComputersByUser_ShouldOnlyReturnComputersOfTheUser()
+        {
+            var context = GetDemoContext();
+            var controller = new ComputersController(context);
+
+            var result = controller.GetComputers("686990B4-3263-4566-9713-27666E894AF3") as TestComputerDbSet;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Local.Count);
+        }
+
+        
+
+        private TestLicenseManagerContext GetDemoContext()
+        {
+            var context = new TestLicenseManagerContext();
+            context.Computers.Add(new Computer
+            {
+                ComputerId = 1,
+                UserId = "686990B4-3263-4566-9713-27666E894AF3",
+                Hostname = "Demo Computer 1",
+                IpAddress = "127.0.0.1",
+                Location = "Demo Location"
+            });
+            context.Computers.Add(new Computer
+            {
+                ComputerId = 2,
+                UserId = "92F7EBC3-9AC1-492F-ADC5-2CFC4348829E",
+                Hostname = "Demo Computer 2",
+                IpAddress = "127.0.0.1",
+                Location = "Demo Location"
+            });
+            context.Computers.Add(new Computer
+            {
+                ComputerId = 3,
+                UserId = "686990B4-3263-4566-9713-27666E894AF3",
+                Hostname = "Demo Computer 3",
+                IpAddress = "127.0.0.1",
+                Location = "Demo location"
+            });
+            return context;
+        }
+
+        private Computer GetDemoComputer()
+        {
+            return new Computer
+            {
+                ComputerId = 4,
+                UserId = "92F7EBC3-9AC1-492F-ADC5-2CFC4348829E",
+                Hostname = "Demo Computer 4",
+                IpAddress = "127.0.0.1",
+                Location = "other location"
+            };
+        }
+    }
+}
---
+++ LicenseManager.Api/Controllers/ComputersController.cs
@@ -0,0 +1,42 @@
+﻿using LicenseManager.Shared;
+using LicenseManager.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LicenseManager.Api.Controllers
+{
+    [RoutePrefix("api/Computers")]
+    public class ComputersController : ApiController
+    {
+        private ILicenseManagerContext _db;
+
+        public ComputersController()
+        {
+            _db = new LicenseManagerContext();
+        }
+
+        public ComputersController(ILicenseManagerContext context)
+        {
+            this._db = context;
+        }
+
+        // GET: api/Computers
+        [Route("api/computers")]
+        public IQueryable<Computer> GetComputers()
+        {
+            // TODO should return only the computers of current user?
+            return _db.Computers;
+        }
+
+        [Route("api/computers/{userId}")]
+        public IQueryable<Computer> GetComputers(string userId)
+        {
+            return _db.Computers.Where(c => c.UserId.Equals(userId, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+    }
+}
